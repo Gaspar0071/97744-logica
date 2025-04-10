c9778c0ba1bb9d145f6acf581c954c296784ad86
Gaspar0071 <igora0476@gmail.com>
2025-04-10 17:09:13 +0000

    salvar

---
+++ 5. atividade.py
@@ -0,0 +1,18 @@
+import os
+os.system ("cls || clear")
+
+
+soma = 0 
+
+def calcular(soma):
+    return soma / 3
+
+
+
+for i in range(3):
+    nota = float(input(Digite uma nota:))
+    soma += nota
+
+
+media = calcular(soma)
+print(f"Media: {media}")
